690e6e5cc39175daad9561ff993ac2abd15c36aa
boddupillivasu <boddupillibujji123@gmail.com>
2024-10-11 19:06:29 +0530

    feat:customerservice implementation

---
+++ src/com/foodiecliapp/service/CustomerServiceImpl.java
@@ -0,0 +1,11 @@
+package com.foodiecliapp.service;
+
+import com.foodiecliapp.exception.CustomerExitsException;
+import com.foodiecliapp.model.Customer;
+
+public class CustomerServiceImpl implements CustomerService{
+    @Override
+    public Customer save(Customer customer) throws CustomerExitsException {
+        return null;
+    }
+}
