0f1ed92f4311c6f3a557e9de247af9da66eafd98
boddupillivasu <boddupillibujji123@gmail.com>
2024-10-11 20:10:10 +0530

    feat:restaurant exception exits class

---
+++ src/com/foodiecliapp/exception/RestaurantExitsException.java
@@ -0,0 +1,7 @@
+package com.foodiecliapp.exception;
+
+public class RestaurantExitsException extends Exception{
+    public RestaurantExitsException(String message) {
+        super(message);
+    }
+}
