5dc0f2172be1dac1e2bb5f428867808213a564ef
boddupillivasu <boddupillibujji123@gmail.com>
2024-10-15 18:16:17 +0530

    Interface:RestaurantService

--- src/com/foodiecliapp/service/RestaurantService.java
+++ src/com/foodiecliapp/service/RestaurantService.java
@@ -1,11 +1,24 @@
 package com.foodiecliapp.service;
 
 import com.foodiecliapp.exception.CustomerExitsException;
+import com.foodiecliapp.exception.DishNotFoundExcepton;
 import com.foodiecliapp.exception.RestaurantExitsException;
+import com.foodiecliapp.exception.RestaurantNotFoundException;
 import com.foodiecliapp.model.Customer;
+import com.foodiecliapp.model.Dish;
 import com.foodiecliapp.model.Restaurant;
+
+import java.util.List;
 
 public interface RestaurantService {
 
     public Restaurant save(Restaurant restaurant) throws RestaurantExitsException;
+    public Restaurant getRestaurantById(String id) throws RestaurantNotFoundException;
+
+    public Restaurant updateRestaurant(Restaurant restaurant) throws RestaurantNotFoundException;
+
+    public void deleteRestaurant(String id) throws RestaurantNotFoundException;
+
+    public List<Dish> getDishItems(String id) throws RestaurantNotFoundException, DishNotFoundExcepton;
 }
+
